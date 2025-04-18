853f36d424e93ea07bc1e5be5de5eb5e03732909
MedSahbiBenKalia <mohamedsahbi.benkalia@insat.ucar.tn>
2025-04-18 23:22:43 +0100

    updating Admin guard

--- src/auth/guards/admin.guard.ts
+++ src/auth/guards/admin.guard.ts
@@ -1,7 +1,8 @@
-import { ExecutionContext } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Role } from "src/enums/role.enum";
 
-export class AdminGuard {
+@Injectable()
+export class AdminGuard implements CanActivate {
     canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest();
         const user = request.user;
